13225979351d5073a8d261a8ef1ac9eb3859af2f
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2023-05-03 20:34:48 +0700

    Second Update

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,21 +6,46 @@
 #include <Adafruit_AHTX0.h>
 
 // library for rain drop sensor
-//#define rainAnalog (analog pin for rain drop data stream)
-//#define rainDigital (digital pin for rain drop data stream)
+#define rainAnalog A1 //(redefined later analog pin for rain drop data stream)
+#define rainDigital 1 //(redefined later digital pin for rain drop data stream)
 
 // Library for sound sensor
-// #define soundAnalog(analog pin for sound data stream)
+#define soundAnalog A2 //(redefined later analog pin for sound data stream)
 
-// Library for 
+// Library for pir sensor
+#define pirAnalog A3 //(redefined later analog pin for pir data stream)
 
+// Interface for infrared sensor
+#define infraredAnalog A4 //(analog pin for infrared data stream)
+#define infraredDigital 2 //(digital pin for infrared data stream)
+
+// Define Maximum Value To Trigger Warning
+
+// create global variable analog for each sensor
+int rainSensorAnalogValue;
+int soundSensorAnalogValue;
+int pirSensorAnalogValue;
+int infraredSensorAnalogValue;
+
+// create global variable digital for each sensor
+int rainSensorDigitalValue;
+int soundSensorDigitalValue;
+int pirSensorDigitalValue;
+int infraredSensorDigitalValue;
+
+// create function for rainDrop algorithm
+void RainDrop(int val1, int val2, int val3) {
+  if
+}
 
 // create function to read data from sesnor
-
+void SecondThreadFunction () {
+  // this function for second thread to read data from sensor
+}
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // declare pin for input
+  
 }
 
 void loop() {
