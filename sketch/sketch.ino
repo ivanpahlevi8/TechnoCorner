cc47da15b12affaa9469643f000f98ec1bff1256
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2023-05-03 19:56:22 +0700

    First Commit

---
+++ sketch/sketch.ino
@@ -0,0 +1,29 @@
+// library for DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// library for AHT-10
+#include <Adafruit_AHTX0.h>
+
+// library for rain drop sensor
+//#define rainAnalog (analog pin for rain drop data stream)
+//#define rainDigital (digital pin for rain drop data stream)
+
+// Library for sound sensor
+// #define soundAnalog(analog pin for sound data stream)
+
+// Library for 
+
+
+// create function to read data from sesnor
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
